cd75941b967990b542746772b4ea5aadc73ac847
VictorGraciano21 <victor_npd@hotmail.com>
2016-11-17 11:52:20 -0700

    Uso basico JQuery, AJAX y css

--- js/eventos.js
+++ js/eventos.js
@@ -7,8 +7,11 @@ var inicio = function(){
   			url: 'https://randomuser.me/api/',
   			dataType: 'json',
   			success: function(data) {
+  				$("#txtNombre").show("slow");
+  				$("#imgFoto").show("slow");
   				$("#txtNombre").val(data.results[0].name.first+" "+data.results[0].name.last);
-  				$("#imgFoto").attr("src",data.results[0].picture.medium);
+  				$("#imgFoto").attr("src",data.results[0].picture.large);
+    			$("#miArticulo").html("Texto");
     			console.log(data.results[0].name.first+" "+data.results[0].name.last);
 		  }
 		});
