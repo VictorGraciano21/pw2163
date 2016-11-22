15cbbaf03662a0e75195f17cdaffd4634745fdbc
VictorGraciano21 <victor_npd@hotmail.com>
2016-11-22 12:03:12 -0700

    PHP guardando en la base de datos

--- ejemplo.php
+++ ejemplo.php
@@ -1,5 +1,6 @@
 <h1>Alta de usuarios</h1>
 <form action="ejemplo.php" method="POST">
+	<!-- <input type="guardaUsuario" name="guardaUsuario" value="guardaUsuraio"> -->
 	<input type="hidden" name="txtOculto" value="guardaUsuario"><br>
 	<input type="text" name="txtUsuario" id="txtUsuario"><br>
 	<input type="text" name="txtNombre" id="txtNombre"><br>
@@ -12,5 +13,47 @@
 	<input type="submit" value="Enviar">
 </form>
 <?php
-	
+	//Preguntar si los valores existen
+	$oculto = ""; //inicializando la variable
+	$usuario = "";
+	$nombre = "";
+	$clave = "";
+	$tipo = "";
+	if (isset($_POST["txtOculto"])) {
+		$oculto = $_POST["txtOculto"];
+	}
+	if (isset($_POST["txtUsuario"])) {
+		$usuario = $_POST["txtUsuario"];
+	}
+	if (isset($_POST["txtNombre"])) {
+		$nombre = $_POST["txtNombre"];
+	}
+	if (isset($_POST["txtClave"])) {
+		$clave = $_POST["txtClave"];
+	}
+	if (isset($_POST["txtTipo"])) {
+		$tipo = $_POST["txtTipo"];
+	}
+	function guardaUsuario($usuario,$nombre,$clave,$tipo){
+		//Conectarse al servidor MySQL
+		//mysql_connect(servidor,usuario,contraseña);
+		$conexion = mysql_connect("localhost","root","");
+		//seleccionamos la BD
+		mysql_select_db("bd2163");
+		$consulta = "insert into usuarios values('".$usuario."','".$nombre."','".$clave."','".$tipo."')";
+		//Ejecutamos la consulta
+		mysql_query($consulta);
+		if(mysql_affected_rows()>0){
+			print("Registro exitoso");
+		}else{
+			print("No se guardo :'c");
+		}
+	}
+	switch ($oculto) {
+		case 'guardaUsuario':
+			guardaUsuario($usuario,$nombre,$clave,$tipo);
+			break;
+		default:
+			break;
+	}
 ?>
